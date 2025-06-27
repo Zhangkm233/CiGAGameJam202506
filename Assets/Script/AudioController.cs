a2ffbba750e58453b78798cbaedbf37717a7d691
3KkM <32219924+Zhangkm233@users.noreply.github.com>
2025-06-27 15:39:08 +0800

    做了一些简单的准备工作
    导入了字体 制作了音乐播放器

---
+++ Assets/Script/AudioController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    //用于播放背景音乐和音效的控制器
+    public AudioClip[] audioClips;
+    public AudioClip hitSfx;
+    public AudioSource sfxSource;
+    public AudioSource bgmSource;
+    public int clipsIndex = 0;
+
+    void Start() {
+        if (bgmSource == null) {
+            bgmSource = this.GetComponent<AudioSource>();
+        }
+        PlayClip();
+    }
+    void Update() {
+        if (!bgmSource.isPlaying) {
+            PlayClip();
+        }
+    }
+    private void PlayClip() {
+        if (audioClips.Length > 0) {
+            if (clipsIndex >= audioClips.Length) {
+                clipsIndex = 0;
+            }
+            bgmSource.clip = audioClips[clipsIndex];
+            bgmSource.Play();
+            clipsIndex++;
+        } else {
+            Debug.LogWarning("audioClips没有音乐");
+        }
+    }
+
+    private void PlaySfx(AudioClip audioclip) {
+        sfxSource.PlayOneShot(audioclip);
+    }
+
+    public void PlayXXX() {
+        
+    }
+}
---
+++ Assets/Script/GameData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public static class GameData
+{
+    //用于存储全局数据，方便各函数调用
+}
+
