9211eeb789e43e806e9d4a731bc32ab2daf9d7c2
Globear <106528728+Globears@users.noreply.github.com>
2025-06-29 21:00:09 +0800

    添加了规则信息

--- Assets/Script/Piece/PieceState/PieceSelectedState.cs
+++ Assets/Script/Piece/PieceState/PieceSelectedState.cs
@@ -6,13 +6,21 @@ public class PieceSelectedState : PieceState
     private List<Vector2Int> _possibleMoves;
 
     public PieceSelectedState(Piece piece) : base(piece) { }
+    
+    public delegate void OnPieceSelected(Piece piece);
+    public static event OnPieceSelected PieceSelectedEvent;
+
+    public delegate void OnPieceDeselected(Piece piece);
+    public static event OnPieceDeselected PieceDeselectedEvent;
 
     public override void OnEnter()
     {
         Debug.Log($"{_piece.name} 进入选中状态.");
         // 突出显示选中棋子，例如改变颜色或添加选中特效
-        _possibleMoves = _piece.GetPossibleMoves(); 
+        _possibleMoves = _piece.GetPossibleMoves();
         BoardManager.Instance?.HighlightMoves(_possibleMoves);
+        // 触发选中事件
+        PieceSelectedEvent?.Invoke(_piece);
     }
 
     public override void OnUpdate()
@@ -26,5 +34,8 @@ public class PieceSelectedState : PieceState
         Debug.Log($"{_piece.name} 退出选中状态.");
         // 退出选中状态时清除高亮显示
         BoardManager.Instance?.ClearHighlights();
+        // 触发取消选中事件
+        PieceDeselectedEvent?.Invoke(_piece);   
+        
     }
 }
--- Assets/Script/UI/GlobalInfoUI.cs
+++ Assets/Script/UI/GlobalInfoUI.cs
@@ -6,10 +6,36 @@ public class GlobalInfoUI : MonoBehaviour
     [SerializeField] private TMPro.TextMeshProUGUI turnText; // 当前回合数文本
     [SerializeField] private TMPro.TextMeshProUGUI newPieceCountDown; // 新棋子倒计时文本
     [SerializeField] private TMPro.TextMeshProUGUI enemySpawnPerTurn; // 每回合生成敌人数量文本
+    [SerializeField] private TMPro.TextMeshProUGUI RightSideGameruleInfo; // 右侧游戏规则信息文本
+    [SerializeField] private TMPro.TextMeshProUGUI LeftSideGameruleInfo; // 左侧游戏规则信息文本
 
-    public void UpdateInfoText(int turn,int countDown,int enemies) {
+    void Start()
+    {
+        PieceSelectedState.PieceSelectedEvent += HideGameRules;
+        PieceSelectedState.PieceDeselectedEvent += ShowGameRules;
+        ShowGameRules(null);
+    }
+
+    public void UpdateInfoText(int turn, int countDown, int enemies)
+    {
         turnText.text = $"当前回合: {turn}"; // 更新回合数文本
         newPieceCountDown.text = $"新棋子倒计时: {countDown}"; // 更新新棋子倒计时文本
         enemySpawnPerTurn.text = $"生成敌人数量: {enemies}"; // 更新敌人生成数量文本
     }
+
+    public void ShowGameRules(Piece piece)
+    {
+        RightSideGameruleInfo.gameObject.SetActive(true);
+        LeftSideGameruleInfo.gameObject.SetActive(true);
+        infoPanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(-5, -437);
+    }
+
+    public void HideGameRules(Piece piece)
+    {
+        RightSideGameruleInfo.gameObject.SetActive(false);
+        LeftSideGameruleInfo.gameObject.SetActive(false);
+        infoPanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(-6, 62);
+    }
+
+    
 }
